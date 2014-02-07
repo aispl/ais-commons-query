ded86ece536dca8cf126adadbd5bfad3a4c451d9
Michał Jaśtak <warlock@ais.pl>
2014-02-07 19:54:15 +0100

    type parameterized; new property added - orderings, with accessor; constructor will allow orderings setting; equals and hashCode methods overridden; toString method added

--- src/main/java/pl/ais/commons/query/AbstractSelection.java
+++ src/main/java/pl/ais/commons/query/AbstractSelection.java
@@ -1,28 +1,33 @@
 package pl.ais.commons.query;
 
-import javax.annotation.concurrent.ThreadSafe;
+import static com.google.common.base.Objects.toStringHelper;
 
-import com.google.common.base.Objects;
-import com.google.common.base.Objects.ToStringHelper;
+import java.io.Serializable;
+import java.util.Arrays;
+
+import javax.annotation.concurrent.ThreadSafe;
 
 /**
  * Class to be extended by all {@linkplain Selection selections}.
  *
+ * @param <R> the type of ordering element
  * @author Warlock, AIS.PL
  * @since 1.0.1
  */
 @SuppressWarnings("PMD.BeanMembersShouldSerialize")
 @ThreadSafe
-public abstract class AbstractSelection implements Selection {
+public abstract class AbstractSelection<R extends Serializable> implements Selection<R> {
 
     /**
      * Identifies the original class version for which it is capable of writing streams and from which it can read.
      *
      * @see <a href="http://docs.oracle.com/javase/7/docs/platform/serialization/spec/version.html#6678">Type Changes Affecting Serialization</a>
      */
-    private static final long serialVersionUID = 2533393473585095184L;
+    private static final long serialVersionUID = 4943985575820732558L;
 
     private final int displayLength;
+
+    private final R[] orderings;
 
     private final int startIndex;
 
@@ -32,7 +37,8 @@ public abstract class AbstractSelection implements Selection {
      * @param startIndex the index of first record
      * @param displayLength the number of records (if {@code -1}, all records will be used)
      */
-    protected AbstractSelection(final int startIndex, final int displayLength) {
+    @SafeVarargs
+    protected AbstractSelection(final int startIndex, final int displayLength, final R... orderings) {
 
         // Verify constructor requirements, ...
         if (startIndex < 0) {
@@ -42,6 +48,21 @@ public abstract class AbstractSelection implements Selection {
         // ... and initialize this instance fields.
         this.startIndex = startIndex;
         this.displayLength = displayLength;
+        this.orderings = Arrays.copyOf(orderings, orderings.length);
+    }
+
+    /**
+     * @see java.lang.Object#equals(java.lang.Object)
+     */
+    @Override
+    public boolean equals(final Object object) {
+        boolean result = (this == object);
+        if (!result && (null != object) && (getClass() == object.getClass())) {
+            final AbstractSelection<?> other = (AbstractSelection<?>) object;
+            result = (startIndex == other.startIndex) && (displayLength == other.displayLength)
+                && Arrays.equals(orderings, other.orderings);
+        }
+        return result;
     }
 
     /**
@@ -53,11 +74,27 @@ public abstract class AbstractSelection implements Selection {
     }
 
     /**
+     * @return the orderings
+     */
+    @Override
+    public R[] getOrderings() {
+        return Arrays.copyOf(orderings, orderings.length);
+    }
+
+    /**
      * {@inheritDoc}
      */
     @Override
     public int getStartIndex() {
         return startIndex;
+    }
+
+    /**
+     * @see java.lang.Object#hashCode()
+     */
+    @Override
+    public int hashCode() {
+        return (31 * ((31 * (31 + startIndex)) + displayLength)) + Arrays.hashCode(orderings);
     }
 
     /**
@@ -69,10 +106,12 @@ public abstract class AbstractSelection implements Selection {
     }
 
     /**
-     * @return new instance of {@link ToStringHelper} for this object, with appended start index and display length
+     * @see java.lang.Object#toString()
      */
-    protected ToStringHelper toStringHelper() {
-        return Objects.toStringHelper(this).add("startIndex", startIndex).add("displayLength", displayLength);
+    @Override
+    public String toString() {
+        return toStringHelper(this).add("startIndex", startIndex)
+            .add("displayLength", displayLength).add("orderings", orderings).toString();
     }
 
 }
